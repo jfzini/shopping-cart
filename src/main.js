607561a1186e540413204f6439f35f02d3c62ea4
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-22 17:05:59 -0300

    starting 4th req

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import './style.css';
 import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 
-const productContainer = document.querySelector('.products');
-const productsList = await fetchProductsList('computador');
-productsList.forEach((product) => productContainer.appendChild(createProductElement(product)));
+const createAllProducts = async () => {
+  const productContainer = document.querySelector('.products');
+  const productsList = await fetchProductsList('computador');
+  productsList.forEach((product) => productContainer.appendChild(createProductElement(product)));
+
+}
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+window.onload = () => {
+  createAllProducts();
+}
