e7b4c08a00372db5ec596e396a443448664abc9c
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-22 17:36:26 -0300

    feat: display error message when rejected
    Now the p element should display an error message if failing to reach the API. Made adjustments to lint

--- src/main.js
+++ src/main.js
@@ -1,23 +1,30 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement, createCustomElement } from './helpers/shopFunctions';
+
 const productContainer = document.querySelector('.products');
 
 const createEachProduct = async (query) => {
   const productsList = await fetchProductsList(query);
-  productsList.forEach((product) => productContainer.appendChild(createProductElement(product)));
-}
+  productsList.forEach((product) => productContainer
+    .appendChild(createProductElement(product)));
+};
 
 const createAllProducts = async (query) => {
   const loadingP = createCustomElement('p', 'loading', 'carregando...');
-  productContainer.appendChild(loadingP);
-  await createEachProduct(query);
-  loadingP.className = 'hidden';
-}
+  try {
+    productContainer.appendChild(loadingP);
+    await createEachProduct(query);
+    loadingP.className = 'hidden';
+  } catch (error) {
+    loadingP.innerHTML = 'Erro ao carregar API';
+  }
+  return null;
+};
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 window.onload = () => {
   createAllProducts('computador');
-}
+};
