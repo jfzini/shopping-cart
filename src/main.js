5efd1971340d51500540a2de70acec9f16725c37
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-22 17:25:27 -0300

    4th req

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { createProductElement, createCustomElement } from './helpers/shopFunctions';
+const productContainer = document.querySelector('.products');
 
-const createAllProducts = async () => {
-  const productContainer = document.querySelector('.products');
-  const productsList = await fetchProductsList('computador');
+const createEachProduct = async (query) => {
+  const productsList = await fetchProductsList(query);
   productsList.forEach((product) => productContainer.appendChild(createProductElement(product)));
+}
 
+const createAllProducts = async (query) => {
+  const loadingP = createCustomElement('p', 'loading', 'carregando...');
+  productContainer.appendChild(loadingP);
+  await createEachProduct(query);
+  loadingP.className = 'hidden';
 }
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 window.onload = () => {
-  createAllProducts();
+  createAllProducts('computador');
 }
