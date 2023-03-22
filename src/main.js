50ee3409257479629892b8bbee2340bd78926aa4
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-22 16:51:29 -0300

    starting 3rd req

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
+
+const productsList = await fetchProductsList('computador');
+productsList.forEach(async (product) => await createProductElement(product));
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
