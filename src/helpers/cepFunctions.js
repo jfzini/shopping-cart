b72c0b1cac5486d630df6b7cfc03cae65cb42157
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-23 00:45:04 -0300

    req 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,18 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (CEP) => {
+  const awesomeAPI = fetch(`https://cep.awesomeapi.com.br/json/${CEP}`);
+  const brasilAPI = fetch(`https://brasilapi.com.br/api/cep/v2/${CEP}`);
+  const returned = await Promise.any([awesomeAPI, brasilAPI]);
+  const data = await returned.json();
+  return data
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const span = document.querySelector('.cart__address');
+  const CEP = document.querySelector('.cep-input').value;
+  try {
+    const { address, district, city, state } = await getAddress(CEP);
+    span.innerHTML = `${address} - ${district} - ${city} - ${state}`
+  } catch (error) {
+    span.innerHTML = 'CEP não encontrado';
+  }
 };
