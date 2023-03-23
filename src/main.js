69ab6408062849a56060cbab6deca36322313c5d
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-22 23:07:50 -0300

    req 9

--- src/main.js
+++ src/main.js
@@ -6,17 +6,17 @@ import {
   createCustomElement,
   createCartProductElement,
 } from './helpers/shopFunctions';
-import { saveCartID } from './helpers/cartFunctions';
+import { saveCartID, getSavedCartIDs } from './helpers/cartFunctions';
 
-// UPPER SCOPE DECLARATIONS
+// ==============UPPER SCOPE DECLARATIONS==============
 const productContainer = document.querySelector('.products');
 const cartProducts = document.querySelector('.cart__products');
 
-// FUNCTIONS
+// ==============FUNCTIONS==============
 const createEachProduct = async (query) => {
   const productsList = await fetchProductsList(query);
-  productsList.forEach((product) => productContainer
-    .appendChild(createProductElement(product)));
+  productsList
+    .forEach((product) => productContainer.appendChild(createProductElement(product)));
 };
 
 const createAllProducts = async (query) => {
@@ -31,18 +31,36 @@ const createAllProducts = async (query) => {
   }
 };
 
+// add selected elements to cart and save its IDs on local storage
+const addToCart = async (e) => {
+  const ID = e.target.parentElement.firstElementChild.innerHTML;
+  const productData = await fetchProduct(ID);
+  const li = createCartProductElement(productData);
+  saveCartID(ID);
+  cartProducts.appendChild(li);
+};
+
+// get the ID's saved on the local storage and restore cart in the same order using Promise.all
+const restoreCart = async () => {
+  const storedArr = getSavedCartIDs();
+  const promises = storedArr.map((ID) => fetchProduct(ID));
+  const productDataArr = await Promise.all(promises);
+  productDataArr.forEach((product) => {
+    const li = createCartProductElement(product);
+    cartProducts.appendChild(li);
+  });
+};
+
 // EVENT LISTENERS
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-// ONLOAD FUNCTIONS
+// ==============ONLOAD FUNCTIONS==============
 window.onload = async () => {
-  await createAllProducts('computador');
+  await createAllProducts('computador'); // Load the page with a set of computers fetched from the API
   const addToCartBtnsArr = document.querySelectorAll('.product__add');
-  addToCartBtnsArr.forEach((btn) => btn.addEventListener('click', async (e) => {
-    const ID = e.target.parentElement.firstElementChild.innerHTML;
-    const productData = await fetchProduct(ID);
-    const li = createCartProductElement(productData);
-    saveCartID(ID);
-    cartProducts.appendChild(li);
-  }));
+
+  // add an eventListener for each button after they're created by the createAllProducts function
+  addToCartBtnsArr.forEach((btn) => btn.addEventListener('click', addToCart));
+
+  restoreCart();
 };
